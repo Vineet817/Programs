05bbdc00bdbeee15c667ed7423997f6e91bab337
Vineet kumar <vineetgarg817@gmail.com>
2025-04-03 15:35:09 +0530

    day9

--- Rust/add-binary.rs
+++ Rust/add-binary.rs
@@ -1,3 +1,8 @@
 pub fn add_binary(a: String, b: String) -> String {
-
+    let num1 = i32::from_str_radix(&a, 2).unwrap();
+    let num2 = i32::from_str_radix(&b, 2).unwrap();
+    format!("{:b}", num1 + num2)
+}
+fn main() {
+    println!("{}",add_binary("1".to_string(), "1".to_string()));
 }
